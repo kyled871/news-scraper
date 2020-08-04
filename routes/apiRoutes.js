8a7fe247cb785be3c88ec753145e2e0071c6a56c
Kyle Davis <kyled871@yahoo.com>
2020-08-04 19:45:31 -0400

    added html with routes/ created Models/ styled page

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,7 @@
 var cheerio = require('cheerio');
 var axios = require('axios');
+var db = require('../models');
 
 module.exports = function(app) {
-    
+
 }
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,5 +1,13 @@
 var path = require('path');
 
 module.exports = function(app) {
-    
+
+    app.get('/', function(req, res) {
+        res.sendFile(path.join(__dirname, '../public/html/index.html'))
+    });
+
+    app.get('/saved', function(req, res) {
+        res.sendFile(path.join(__dirname, '../public/html/saved.html'))
+    });
+
 }
